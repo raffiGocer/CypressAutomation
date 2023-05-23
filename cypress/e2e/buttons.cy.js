d073a3ac6923d7710e097cf26e00877e15b22dae
RaffiGocer <raffigocer@gmail.com>
2023-05-23 09:48:17 -0400

    add: new locator strategies

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -12,6 +12,14 @@ describe('Context: My First Test',() => {
         // select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
+
+        //find element with class attribute and create a list then select 3rd element from the list
+
+        cy.get('.btn.btn-primary').then(($buttons) => {
+            cy.wrap($buttons).eq(2).click(); // zero indexed list
+            // assert the text
+            cy.contains('Clicked on button three!').should('be.visible');
+        })
         
     })
 })
